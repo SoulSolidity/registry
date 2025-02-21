57f5883861d58a6b77b9674b4cae130d601eeb66
Doublo <doublo54@gmail.com>
2025-02-21 11:53:01 +0100

    feat: added chain and dex images

---
+++ src/constants/constants.ts
@@ -0,0 +1,12 @@
+//This is used on SoulSolidity.com pricing page to show the supported chain images
+export const priceGetterSupportedChainsImages = [
+    { name: 'BNB', image: 'bnb.svg' },
+    { name: 'Ethereum', image: 'ethereum.svg' },
+    { name: 'Polygon', image: 'polygon.svg' },
+    { name: 'Linea', image: 'linea.svg' },
+    { name: 'Base', image: 'base.svg' },
+    { name: 'Avalanche', image: 'avalanche.svg' },
+    { name: 'Blast', image: 'blast.svg' },
+    { name: 'Lightlink', image: 'lightlink.svg' },
+    { name: 'IOTA', image: 'iota.svg' }
+]
