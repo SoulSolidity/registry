0824c278e756aafb7d8f02358239d2fd6f505200
Doublo <doublo54@gmail.com>
2025-02-21 12:09:43 +0100

    feat: updated chains and added token images

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,7 +6,30 @@ export const priceGetterSupportedChainsImages = [
     { name: 'Linea', image: 'linea.svg' },
     { name: 'Base', image: 'base.svg' },
     { name: 'Avalanche', image: 'avalanche.svg' },
-    { name: 'Blast', image: 'blast.svg' },
-    { name: 'Lightlink', image: 'lightlink.svg' },
-    { name: 'IOTA', image: 'iota.svg' }
+    { name: 'Arbitrum', image: 'arbitrum.svg' },
+    { name: 'BeraChain', image: 'bera.svg' },
+    { name: 'BeraChain', image: 'bera.svg' },
+]
+
+//This is used on SoulSolidity.com pricing page to show the playground tokens
+//Images from https://github.com/trustwallet/assets/tree/master/blockchains
+export const priceGetterPlaygroundTokens = [
+    {
+        name: "Coinbase Wrapped BTC",
+        address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+        chainId: 8453,
+        icon: "https://raw.githubusercontent.com/trustwallet/assets/refs/heads/master/blockchains/bitcoin/info/logo.png"
+    },
+    {
+        name: "Wrapped Ethereum",
+        address: "0x940181a94A35A4569E4529A3CDfB74e38FD98631",
+        chainId: 8453,
+        icon: "https://raw.githubusercontent.com/trustwallet/assets/refs/heads/master/blockchains/ethereum/info/logo.png"
+    },
+    {
+        name: "USD Coin",
+        address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+        chainId: 8453,
+        icon: "https://raw.githubusercontent.com/trustwallet/assets/refs/heads/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png"
+    }
 ]
