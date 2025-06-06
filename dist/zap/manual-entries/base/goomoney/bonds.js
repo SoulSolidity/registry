1080e5e5c578dff7f5a60e651c6ec24477333954
Doublo <83957749+Doublo54@users.noreply.github.com>
2025-06-06 12:53:35 +0200

    chore: add first goo bond

--- dist/zap/manual-entries/base/goomoney/bonds.js
+++ dist/zap/manual-entries/base/goomoney/bonds.js
@@ -1,4 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.gooMoneyBonds = void 0;
-exports.gooMoneyBonds = [];
+const types_1 = require("../../../types");
+exports.gooMoneyBonds = [
+    {
+        name: "GooMoney GOO Bond",
+        address: "0x1A859941D9c2380d0d5578DEF60F8C4BB96D751F",
+        type: types_1.LPType.SINGLE,
+        inputToken: {
+            name: "cbBTC",
+            address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+        },
+    },
+];
--- src/zap/manual-entries/base/goomoney/bonds.ts
+++ src/zap/manual-entries/base/goomoney/bonds.ts
@@ -1,5 +1,13 @@
 import { ProtocolEntry, LPType, Project } from '../../../types';
 
 export const gooMoneyBonds: ProtocolEntry[] = [
-
+    {
+        name: "GooMoney GOO Bond",
+        address: "0x1A859941D9c2380d0d5578DEF60F8C4BB96D751F",
+        type: LPType.SINGLE,
+        inputToken: {
+            name: "cbBTC",
+            address: "0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf",
+        },
+    },
 ];
